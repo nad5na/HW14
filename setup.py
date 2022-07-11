1cdfa85742a03476cf7861153064757e71c9bab1
nad5na <107503039+nad5na@users.noreply.github.com>
2022-07-11 15:42:30 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Book',
+      version='0.1',
+      description='making a book module',
+      url='https://github.com/nad5na/HW14',
+      author='Noah Dunn',
+      author_email='nad5na@virginia.edu',
+      license='MIT',
+      packages=['Book'])
